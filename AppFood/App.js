2d0c4218a574aea433186059515ffbe68d96d7fb
Phan Phong <phanphongspvp@gmail.com>
2022-11-27 10:36:07 +0700

    first commit

--- AppFood/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import AppNavigation from './src/navigation'
-
-export default function App() {
-  return (
-    <AppNavigation />
-  )
-}
--- AppFood/src/navigation/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Slider from '../components/Slider'
-
-export default function AppNavigation() {
-    return (
-        <Slider />
-    )
-}
